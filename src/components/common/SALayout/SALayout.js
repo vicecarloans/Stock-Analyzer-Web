b94992caf1a4ec17e21045487a2adf0b9955af6c
vicecarloans <huyst1997@icloud.com>
2019-02-05 19:32:08 -0500

    Using unencrypted heroku

---
+++ src/components/common/SALayout/SALayout.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from "react";
+import { LinearGradient } from "./SALayout.styles";
+
+export default class SALayout extends PureComponent {
+  render() {
+    return (
+      <div>
+        <LinearGradient />
+      </div>
+    );
+  }
+}
---
+++ src/components/common/SALayout/SALayout.styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const LinearGradient = styled.div`
+  background-image: linear-gradient(#141e30, #000);
+  width: 100vw;
+  max-height: 100px;
+`;
