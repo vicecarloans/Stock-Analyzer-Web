4084df05756bb9fd5beda206d9d66c3859bbdb3e
vicecarloans <huyst1997@icloud.com>
2019-02-14 05:02:30 -0500

    Eliminate a bug where token is null

--- server/middlewares/requireLogin.js
+++ server/middlewares/requireLogin.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const SessionModel = mongoose.model("sessions");
 
 module.exports = async (req, res, next) => {
-  if (!req.cookies || !req.cookies.session_id) {
+  const data = await SessionModel.findById(req.cookies.session_id);
+  if (!data) {
     const error = new Error("Unauthorized");
     error.status = 401;
     next(error);
   }
-  const { token } = await SessionModel.findById(req.cookies.session_id);
-  req.token = token;
+  req.token = data.token;
   next();
 };
