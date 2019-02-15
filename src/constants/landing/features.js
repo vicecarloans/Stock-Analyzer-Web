69c5372d55ecf536e4009b5b235b1a924839320c
Allan Martinez <allanrodrigo.martinezmalagon@georgebrown.ca>
2019-02-14 19:24:32 -0500

    Description of Product
    Product Description for each individual feature in the application.

--- src/constants/landing/features.js
+++ src/constants/landing/features.js
@@ -3,28 +3,28 @@ export const FEATURES = [
     id: 1,
     title: "Manage Earnings",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla",
+      "This feature allows our customers forecast their current profits and refactor their earnings in order to plan their future investments based on educated predictions. Customers will be able to implement forecasting tools in order to view current and predict their future earnings based on investment decisions made within different time frames, controlling their wealth and providing a clear vision on profitability",
     className: "earning-icon"
   },
   {
     id: 2,
     title: "Analyze Stocks",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla",
+      "This feature is the key strenght of our product as it allows users to predict stock patter behaviour based on unique and rare market indicators unopened to the public as of today. Through this feature, our customers will graphically view the performance of any stock in the NADSAQ 100 market in order to shape their investments based on real time data constantly updated to guarantee the best performance predictions.",
     className: "analyze-stock-icon"
   },
   {
     id: 3,
     title: "Real-time Market",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla",
+      "This feature is powered by a unique data feeder which instantaneously deliveres NASDAQ 100 stock information to your desktop directly from the official NASDAQ stock market. Our server is on a constant look-out in order to provide the most up-to date data in regard of stock performance behaviour and information.",
     className: "real-time-market-icon"
   },
   {
     id: 4,
     title: "News Feed",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla",
+      "This feature will provide our customers with the most current news related to every single stock listed in the NASDAQ 100 market. Users will be presented with a variety of newspaper and magainze articles, internet extracts, and any sort of stock analysis found on the internet for user search and referal.",
     className: "news-icon"
   }
 ];
