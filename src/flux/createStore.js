24d81210334cac96c19417656880b3e6b80d77fc
vicecarloans <huyst1997@icloud.com>
2019-02-15 05:24:15 -0500

    Change Redux to work on development only

--- src/flux/createStore.js
+++ src/flux/createStore.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import { persistStore, persistReducer, createTransform } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import createSagaMiddleware from "redux-saga";
