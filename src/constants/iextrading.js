5b36779796ea565389c8cf9de742181b4eb6cdcf
vicecarloans <huyst1997@icloud.com>
2019-03-21 00:31:56 -0400

    Get configVar from next.config.js

--- src/constants/iextrading.js
+++ src/constants/iextrading.js
@@ -1,5 +1,9 @@
+import getConfig from "next/config";
+
+const { publicRuntimeConfig } = getConfig();
+const { IEXTRADING_PUBLISHABLE_TOKEN } = publicRuntimeConfig;
 export const BASE_URL = "https://cloud.iexapis.com/beta";
-export const TOKEN = process.env.IEXTRADING_PUBLISHABLE_TOKEN;
+export const TOKEN = IEXTRADING_PUBLISHABLE_TOKEN;
 
 // export const BASE_URL = 'https://sandbox.iexapis.com';
 // export const TOKEN = process.env.IEXTRADING_TEST_PUBLISHABLE_TOKEN;
